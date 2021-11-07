6fb6f4749bbd1b589116916f8eafa3ea3108522a
AZ <agnieszkayd@gmail.com>
2021-11-07 22:59:00 +0100

    * refactored Screen component * added subtotal & operation to the model

--- UCalc.react.js
+++ UCalc.react.js
@@ -8,19 +8,24 @@ class CalcController {
         } );
     }
 
-    static calcClearPressed() {
-        store.dispatch( {
-            type: 'SET_NUMBER',
-            number: 0
-        } );
-    }
-
-    static calcOperationPressed( operation ) {
+    static calcOperationPressed( operation, ) {
         store.dispatch( {
             type: 'SET_OPERATION',
             operation: operation,
         } );
     }
+
+    static calcClearPressed() {
+        store.dispatch( {
+            type: 'SET_NUMBER',
+            number: 0,
+        } );
+        store.dispatch( {
+            type: 'SET_SUBTOTAL',
+            subtotal: null,
+        } );
+    }
+
 }
 
 class NumericButton extends React.Component {
@@ -72,19 +77,35 @@ class Keyboard extends React.Component {
                     <OperationButton key={s} operation={s} />
                 ),
             // <TotalButton />,
-            <ClearScreen />,
+            <ClearScreen key="C/CE" />,
         ];
     }
 }
 
+// let subtotal = function UpdateSubtotal() {
+//     const number = ReactRedux.useSelector( ( state ) => state.number );
+//     const operation = ReactRedux.useSelector( ( state ) => state.operation );
+//     if ( this.state.number !== null ) {
+//         this.state.subtotal = number + operation;
+//     }
+// };
 
 function Screen() {
     const number = ReactRedux.useSelector( ( state ) => state.number );
-
+    const operation = ReactRedux.useSelector( ( state ) => state.operation );
+    const subtotal = number + operation;
     return (
-        <div>{number}</div>
+        <div>
+            <div key="subtotal">
+                {subtotal}
+            </div>
+            <div key="input">
+                {number}
+            </div>
+        </div>
     );
 }
+
 
 class Calc extends React.Component {
     render() {
@@ -111,6 +132,11 @@ function updateStore( state = {}, action ) {
                 ...state, // object spread
                 operation: action.operation,
             };
+        case 'SET_SUBTOTAL':
+            return {
+                ...state, // object spread
+                operation: action.subtotal,
+            };
         default:
             return state;
     }
@@ -119,6 +145,8 @@ function updateStore( state = {}, action ) {
 const initialState = {
     number: 0,
     operation: null,
+    subtotal: null,
+    opWasLast: false,
 };
 const store = Redux.createStore( updateStore, initialState );
 ReactDOM.render( <Calc />, document.getElementsByTagName( 'calc-main' )[0] );
