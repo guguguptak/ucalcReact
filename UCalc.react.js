862b998985a7a54e6a1d1f1ef3068c2b10ac4be3
AZ <agnieszkayd@gmail.com>
2021-11-19 20:53:08 +0100

    *added doOperation() & stopRepeat() function to the CalcController *added new functionalities to calcOperationPressed *refactored initialState, CalcController

--- UCalc.react.js
+++ UCalc.react.js
@@ -1,28 +1,118 @@
 'use strict';
 
+const PRECISION_MAX = 10;
+const MIN_NON_EXPONENTIAL = Math.pow( 10, -PRECISION_MAX );
+const MAX_NON_EXPONENTIAL = Math.pow( 10, PRECISION_MAX );
+const JS_DUMB_TOSTRING_EXP_THRESHOLD = 1E-6;
+
 const SET_RESULT_ACTION = 'SET_RESULT';
-const SET_OPERATION_ACTION = 'SET_OPERATION';
 const SET_SUBTOTAL_ACTION = 'SET_SUBTOTAL';
+const SET_OPWASLAST_ACTION = 'SET_OPERATION_WAS_LAST';
 
 const SET_STATE_ACTION = 'SET_STATE';
 
 class CalcController {
     static handleNumberPressed( buttonNumber ) {
+        CalcController.stopRepeat();
+
+        const state = store.getState();
+        const newState = {
+            opWasLast: false,
+        };
+
+        if ( state.opWasLast ) {
+            newState.result = buttonNumber;
+        } else {
+            if ( state.result < 0 || Object.is( state.result, -0 ) ) {
+                buttonNumber *= -1;
+            }
+            if ( state.dotPosition === null ) {
+                newState.fakeZeroes = 0;
+                newState.result = state.result * 10 + buttonNumber;
+            } else {
+                if ( state.dotPosition < PRECISION_MAX ) {
+                    newState.dotPosition++;
+                    if ( buttonNumber === 0 ) {
+                        newState.fakeZeroes++;
+                        // updateInput();
+                    } else {
+                        newState.fakeZeroes = 0;
+                        newState.result = state.result + buttonNumber / Math.pow( 10, state.dotPosition );
+                    }
+                }
+            }
+        }
+
         store.dispatch( {
-            type: SET_RESULT_ACTION,
-            result: store.getState().result * 10 + buttonNumber,
+            type: SET_STATE_ACTION,
+            newState: newState,
         } );
     }
 
-    static calcOperationPressed( operation, ) {
+    static stopRepeat() {
+        const newState = {
+            repeatValue: null,
+        };
+        if ( store.getState().subtotal === null ) {
+            newState.lastOp = null;
+        }
         store.dispatch( {
             type: SET_STATE_ACTION,
-            newState: {
-                operation: operation,
-                subtotal: store.getState().result,
-                result: 0,
-            },
+            newState: newState,
         } );
+    }
+
+    static doOperation() {
+        const state = store.getState();
+        let newResult = state.subtotal;
+        switch ( state.lastOp ) {
+            case '+' :
+                newResult += state.result;
+                break;
+            case '-':
+                newResult -= state.result;
+                break;
+            case '*':
+                newResult *= state.result;
+                break;
+            case '/':
+                newResult /= state.result;
+                break;
+            case null:
+                break;
+            default:
+                throw new Error( 'invalid operation' );
+        }
+        const newState = {
+            result: newResult,
+            // lastOp: null,
+            opWasLast: true,
+            dotPosition: null,
+            fakeZeroes: 0,
+        };
+
+        return newState;
+    }
+
+    static calcOperationPressed( op ) {
+        const state = store.getState();
+        const newState = state.opWasLast ? {} : CalcController.doOperation();
+
+        newState.lastOp = op;
+        newState.opWasLast = true;
+        newState.dotPosition = null;
+
+        if ( state.repeatValue === null ) {
+            newState.subtotal = state.result;
+        }
+        store.dispatch( {
+            type: SET_STATE_ACTION,
+            newState: newState,
+        } );
+    }
+
+    static calcTotalPressed() {
+        // selectOperation();
     }
 
     static calcClearPressed() {
@@ -31,6 +121,7 @@ class CalcController {
             newState: {
                 result: 0,
                 subtotal: null,
+                lastOp: null,
             },
         } );
     }
@@ -57,7 +148,6 @@ class OperationButton extends React.Component {
     }
 }
 
-//
 class TotalButton extends React.Component {
     render() {
         return (
@@ -86,22 +176,22 @@ class Keyboard extends React.Component {
                 .map( s =>
                     <OperationButton key={s} operation={s} />
                 ),
-            // <TotalButton />,
+            <TotalButton key="=" />,
             <ClearScreen key="C/CE" />,
         ];
     }
 }
 
 function Screen() {
-    const [result, operation, subtotal] = [
+    const [result, lastOp, subtotal] = [
         ( state ) => state.result,
-        ( state ) => state.operation,
+        ( state ) => state.lastOp,
         ( state ) => state.subtotal,
     ].map( ( x ) => ReactRedux.useSelector( x ) );
     return (
         <div>
             <div id="subtotal">
-                {subtotal} {operation}
+                {subtotal} {lastOp}
             </div>
             <div id="input">
                 {result}
@@ -136,15 +226,15 @@ function updateStore( state = {}, action ) {
                 ...state, // object spread
                 result: action.result,
             };
-        case SET_OPERATION_ACTION:
-            return {
-                ...state, // object spread
-                operation: action.operation,
-            };
         case SET_SUBTOTAL_ACTION:
             return {
                 ...state, // object spread
                 subtotal: action.subtotal,
+            };
+        case SET_OPWASLAST_ACTION:
+            return {
+                ...state, // object spread
+                opWasLast: action.opWasLast,
             };
         default:
             return state;
@@ -153,9 +243,14 @@ function updateStore( state = {}, action ) {
 
 const initialState = {
     result: 0,
-    operation: null,
     subtotal: null,
+    lastOp: null,
     opWasLast: false,
+    dotPosition: null,
+    fakeZeroes: 0,
+    memory: null,
+    repeatValue: null,
 };
+
 const store = Redux.createStore( updateStore, initialState );
 ReactDOM.render( <Calc />, document.getElementsByTagName( 'calc-main' )[0] );
