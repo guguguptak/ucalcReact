b5cfc388ee1c601350e06bffebfe4ebf201f6086
AZ <agnieszkayd@gmail.com>
2021-11-29 17:03:11 +0100

    added calcReact.css & newReactCalcStyle.css

--- react/view.react.js
+++ react/view.react.js
@@ -6,6 +6,15 @@
 // import { initialState } from './model.react.js';
 
 /*export*/
+class CalcButton extends React.Component {
+    render() {
+        return (
+            <button style={this.props.customStyle} onClick={this.props.func}>{this.props.caption}</button>
+        );
+    }
+}
+
+/*export*/
 class NumericButton extends React.Component {
     render() {
         return (
@@ -28,75 +37,6 @@ class OperationButton extends React.Component {
 }
 
 /*export*/
-class TotalButton extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.calcTotalPressed()}>=</button>
-        );
-    }
-}
-
-/*export*/
-class ClearScreen extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.calcClearPressed()}>C/CE</button>
-        );
-    }
-}
-
-/*export*/
-class MemoryStore extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.memoryStorePressed()}>MS</button>
-        );
-    }
-}
-
-/*export*/
-class MemoryRecall extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.memoryRecallPressed()}>MR</button>
-        );
-    }
-}/*export*/
-class MemoryClear extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.memoryClearPressed()}>MC</button>
-        );
-    }
-}/*export*/
-class MemoryAdd extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.memoryAddPressed()}>M+</button>
-
-        );
-    }
-}
-
-/*export*/
-class SignButton extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.signPressed()}>&plusmn;</button>
-        );
-    }
-}
-
-/*export*/
-class DotButton extends React.Component {
-    render() {
-        return (
-            <button onClick={() => CalcController.dotPressed()}>.</button>
-        );
-    }
-}
-
-/*export*/
 class Keyboard extends React.Component {
     render() {
         return [
@@ -109,14 +49,19 @@ class Keyboard extends React.Component {
                 .map( s =>
                     <OperationButton key={s} operation={s} />
                 ),
-            <TotalButton key="=" />,
-            <ClearScreen key="C/CE" />,
-            <MemoryClear key="MC" />,
-            <MemoryStore key="MS" />,
-            <MemoryRecall key="MR" />,
-            <MemoryAdd key="M+" />,
-            <DotButton key="dot" />,
-            <SignButton key="sign" />,
+            <CalcButton func={() => CalcController.calcTotalPressed()} caption="=" key="=" />,
+            <CalcButton func={() => CalcController.calcClearPressed()}
+                        caption="C/CE" key="C/CE" />,
+            <CalcButton func={() => CalcController.memoryClearPressed()}
+                        customStyle={{ gridColumn: 1, gridRow: 2, }} caption="MC" key="MC" />,
+            <CalcButton func={() => CalcController.memoryStorePressed()}
+                        customStyle={{ gridColumn: 3, gridRow: 2, }} caption="MS" key="MS" />,
+            <CalcButton func={() => CalcController.memoryRecallPressed()}
+                        customStyle={{ gridColumn: 2, gridRow: 2, }} caption="MR" key="MR" />,
+            <CalcButton func={() => CalcController.memoryAddPressed()}
+                        customStyle={{ gridColumn: 4, gridRow: 2, }} caption="M+" key="M+" />,
+            <CalcButton func={() => CalcController.dotPressed()} caption="." key="dot" />,
+            <CalcButton func={() => CalcController.signPressed()} caption="&plusmn;" key="sign" />,
         ];
     }
 }
@@ -129,7 +74,7 @@ function Screen() {
         ( state ) => state.subtotal,
     ].map( ( x ) => ReactRedux.useSelector( x ) );
     return (
-        <div>
+        <div id="screen">
             <div id="subtotal">
                 {subtotal} {lastOp}
             </div>
@@ -145,7 +90,7 @@ class Calc extends React.Component {
     render() {
         return (
             < ReactRedux.Provider store={store}>
-                <div>
+                <div className="calc-grid">
                     <Screen />
                     <Keyboard />
                 </div>
