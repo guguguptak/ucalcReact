49cf19060c403a4d04875e8a2ec9e4118f8f1dda
AZ <agnieszkayd@gmail.com>
2022-01-28 13:29:15 +0100

    *added keyboard event (onkeydown) in main.react.js

--- react/main.react.js
+++ react/main.react.js
@@ -2,7 +2,36 @@
 //     initialState,
 //     updateStore
 // } from './model.react.js';
-// import { Calc } from './view.react.js';
 
 const store = Redux.createStore( updateStore, initialState );
 ReactDOM.render( <Calc />, document.getElementsByTagName( 'calc-main' )[0] );
+const keymap = {
+
+    'Enter': () => CalcController.calcTotalPressed(),
+    'Escape': () => CalcController.calcClearPressed(),
+    // 'Backspace': () => undoInput(),
+    '.': () => CalcController.dotPressed(),
+    ',': () => CalcController.dotPressed(),
+    'v': async ( evt ) => await pasteFromClipboard( evt ),
+};
+for ( let i = 0; i <= 9; i++ ) {
+    keymap['' + i] = () => CalcController.handleNumberPressed( i );
+}
+const opMap = {
+    'plus': '+',
+    'minus': '-',
+    'times': '*',
+    'divide': '/',
+};
+for ( const [k, v] of Object.entries( opMap ) ) {
+    keymap[v] = () => CalcController.calcOperationPressed( v );
+}
+document.getElementById( 'calc-body' ).addEventListener( 'keydown', ( evt ) => {
+    const action = keymap[evt.key];
+    if ( action === undefined ) {
+        return;
+    }
+    action( evt );
+    evt.preventDefault();
+   
+} );
